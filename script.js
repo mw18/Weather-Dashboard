2734735cc2aa766eb2dbf8575e34e1bbfc3a8067
mw18 <67554339+mw18@users.noreply.github.com>
2020-11-30 12:49:43 -0600

    append one city under city search

--- script.js
+++ script.js
@@ -6,23 +6,27 @@ var temp = document.querySelector('.temp');
 var humidity = document.querySelector('.humidity');
 var wind = document.querySelector('.wind');
 var button= document.querySelector('.submit');
+var submissionResponseEl = document.querySelector("#response");
+
 
 //-----------------------------Function for searching weather by city---------//
 
 //var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=+input.value+'&units=imperial'+'&appid='+APIKey";
 
-button.addEventListener('click', function(name){
-    fetch('https://api.openweathermap.org/data/2.5/weather?q='+input.value+'&units=imperial'+'&appid='+APIKey)
+
+
+button.addEventListener('click', function(event){
+    fetch('https://api.openweathermap.org/data/2.5/weather?q='+input.value + '&units=imperial'+'&appid='+APIKey)
     .then(function(response) {
         return response.json();
-      })
+      }) 
 
     .then(function(data) {
       var tempValue = data['main']['temp'];
       var nameValue = data['name'];
       var humidityValue = data ['main']['humidity'];
       var windValue = data ['wind'] ['speed'];
-    
+
       main.innerHTML = nameValue;
       temp.innerHTML = "Temperature : "+tempValue+"°F ";
       humidity.innerHTML = "Humidity : "+humidityValue+"%";
@@ -34,5 +38,14 @@ button.addEventListener('click', function(name){
         console.log(error)
       });
 
-    })
+  var response = input.value 
+  submissionResponseEl.textContent = response;
 
+  // fetch ("https://api.openweathermap.org/data/2.5/forecast?q=" + input.value + '&units=imperial'+'&appid='+APIKey)
+  // .then(function(response) {
+  //   return response.json();
+  // }) 
+
+  // var results = response.list;
+
+})
