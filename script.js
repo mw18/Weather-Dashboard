99bc6ef8fb6484b087f30c586057ee66904991fa
mw18 <67554339+mw18@users.noreply.github.com>
2021-01-16 18:14:19 -0600

    update html file, script with functioning five day forecast, and add moment.js file to format dates

--- script.js
+++ script.js
@@ -2,11 +2,15 @@
 var APIKey = "b663a62f70241903dcba8316815769b7";
 var input = document.querySelector('.input_text');
 var main = document.querySelector('#name');
+var forecast = document.querySelector('#forecast');
 var temp = document.querySelector('.temp');
 var humidity = document.querySelector('.humidity');
 var wind = document.querySelector('.wind');
 var button= document.querySelector('.submit');
 var submissionResponseEl = document.querySelector("#response");
+
+//moment.js to format date 
+$("#currentDay").text(moment().format('MMMM Do YYYY, h:mm a'));
 
 
 //-----------------------------Function for searching weather by city---------//
@@ -29,7 +33,7 @@ button.addEventListener('click', function(event){
       var windValue = data ['wind'] ['speed'];
 
       main.innerHTML = nameValue;
-      temp.innerHTML = "Temperature : "+tempValue+"°F ";
+      temp.innerHTML = "Temperature : "+tempValue+ "°F ";
       humidity.innerHTML = "Humidity : "+humidityValue+"%";
       wind.innerHTML = "Wind Speed : " +windValue+"MPH";
       input.value ="";
@@ -38,15 +42,39 @@ button.addEventListener('click', function(event){
     .catch(function(error) {
         console.log(error)
       });
+  
+   event.preventDefault();
+     fetch('https://api.openweathermap.org/data/2.5/forecast?q=' + input.value + '&units=imperial' + '&appid=' + APIKey)
+     .then(function(response) {
+         return response.json()
+        .then((data) => {console.log(data)
+          var days = data.list
+          for (let i = 0; i < 5; i++){
 
-  var response = input.value 
-  submissionResponseEl.textContent = response;
+              var column = $("<div>").addClass("col-sm-2");
+              var card = $("<div>").addClass("card");
+              var cardBod = $("<div>").addClass("card-body");
+              var cityDate = $("<h6>").addClass("card-title").text(moment(days[i * 8].dt_txt).format("MM/DD/YYYY"));
+              var temp = $("<p>").text("Temperature : " + days[i * 8].main.temp.toFixed()+"°F ");
+              var humidity = $("<p>").text("Humidity : "+days[i * 8].main.humidity+"%");
+              $("#forecast").append(column.append(card.append(cardBod.append(cityDate,temp,humidity))))
+          }
 
-  // fetch ("https://api.openweathermap.org/data/2.5/forecast?q=" + input.value + '&units=imperial'+'&appid='+APIKey)
-  // .then(function(response) {
-  //   return response.json();
-  // }) 
 
-  // var results = response.list;
 
-})
+
+        });         
+
+       }); 
+
+
+
+
+var response = input.value 
+submissionResponseEl.textContent = response;
+
+var data = input.value 
+submissionResponseEl.textContent = data;
+
+
+     })
